b326e0e0786ddea7edcc502593b625210d2eb509
Tudor Andrei <T.Andrei@student.tudelft.nl>
2022-06-12 11:56:12 +0200

    Implemented responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <NavBar/>
+    </div>
   );  
 }
 
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container-lg">
+            <a href="#" className="navbar-brand">Home</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navmenu">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a href="#AboutMe" className="nav-link">About me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#Projects" className="nav-link">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#Contact" className="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
